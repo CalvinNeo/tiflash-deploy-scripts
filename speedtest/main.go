fe31f309569ebd99564f04270fac26762b343e34
CalvinNeo <calvinneo1995@gmail.com>
2021-12-08 16:32:52 +0800

    fix
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- speedtest/main.go
+++ speedtest/main.go
@@ -9,29 +9,27 @@ import (
 	"time"
 )
 
-func main() {
+func GetDB() *sql.DB {
 	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:4000)/")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
-	_,err = db.Exec("DROP DATABASE IF EXISTS test99")
+	_, err = db.Exec("DROP DATABASE IF EXISTS test99")
 	if err != nil {
 		panic(err)
 	}
 
-	_,err = db.Exec("CREATE DATABASE test99")
+	_, err = db.Exec("CREATE DATABASE test99")
 	if err != nil {
 		panic(err)
 	}
+	return db
+}
 
-	C := 100
-
-	ctx := context.Background()
-	threads := 10
-
-	ch := make(chan string, threads+10)
+func AsyncStmtEx(ctx context.Context, name string, threads int, db *sql.DB, insert func(*chan []string), checker func(*[]string)) {
+	start := time.Now()
+	ch := make(chan []string, threads+10)
 	var wg sync.WaitGroup
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
@@ -43,11 +41,7 @@ func main() {
 				select {
 				case s, ok := <-ch:
 					if ok {
-						fmt.Printf("Handle %v\n", s)
-						_,err = db.Exec(s)
-						if err != nil {
-							panic(err)
-						}
+						checker(&s)
 					} else {
 						return
 					}
@@ -57,60 +51,144 @@ func main() {
 			}
 		}(i)
 	}
-	for i := 0; i < C; i++ {
-		ch <- fmt.Sprintf("create table test99.t%v(z int)", i)
-	}
+	insert(&ch)
 	close(ch)
 	wg.Wait()
+	fmt.Printf("OK %v %v \v", name, time.Since(start))
+}
 
-
-	start := time.Now()
-	ch = make(chan string, threads+10)
-	var wg2 sync.WaitGroup
-	for i := 0; i < threads; i++ {
-		wg2.Add(1)
-		go func(index int) {
-			defer func() {
-				wg2.Done()
-			}()
-			for {
-				select {
-				case s, ok := <-ch:
-					if ok {
-						fmt.Printf("Handle %v\n", s)
-						_,err = db.Exec(s)
-						if err != nil {
-							panic(err)
-						}
-					} else {
-						return
-					}
-				case <-ctx.Done():
-					return
-				}
+func AsyncStmt(ctx context.Context, name string, threads int, db *sql.DB, insert func(*chan []string)) {
+	f := func(s *[]string) {
+		for _, ss := range *s {
+			fmt.Printf("Handle %v\n", ss)
+			_, err := db.Exec(ss)
+			if err != nil {
+				panic(err)
 			}
-		}(i)
+		}
 	}
-	for i := 0; i < C; i++ {
-		ch <- fmt.Sprintf("alter table test99.t%v set tiflash replica 1", i)
-	}
-	close(ch)
-	wg2.Wait()
+	AsyncStmtEx(ctx, name, threads, db, insert, f)
+}
 
+func main() {
+	// Single
+	//TestPerformance(1, 10)
+	// Multi
+	//TODO
+	TestOncal3996()
+}
+
+
+func TestOncal3996() {
+	// ddltiflash will block ddltiflash_victim
+	db := GetDB()
+	defer db.Close()
+
+	MustExec(db, "drop table if exists test99.ddltiflash")
+	MustExec(db, "create table test99.ddltiflash(z int) PARTITION BY RANGE(z) (PARTITION p0 VALUES LESS THAN (10),PARTITION p1 VALUES LESS THAN (20), PARTITION p2 VALUES LESS THAN (30))")
+	MustExec(db, "alter table test99.ddltiflash set tiflash replica 1")
+
+	WaitTableOK(db, "ddltiflash")
+
+	MustExec(db, "drop table if exists test99.ddltiflash_victim")
+	MustExec(db, "create table test99.ddltiflash_victim(z int)")
+
+	lessThan := "40"
+	MustExec(db, "ALTER TABLE test99.ddltiflash ADD PARTITION (PARTITION pn VALUES LESS THAN (%v))", lessThan)
+
+	MustExec(db, "alter table test99.ddltiflash_victim set tiflash replica 1")
+	WaitTableOK(db, "ddltiflash")
+
+}
+
+func MustExec(db *sql.DB, f string, args ...interface{}) {
+	s := fmt.Sprintf(f, args...)
+	_, err := db.Exec(s)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WaitTableOK(db *sql.DB, tbn string) {
 	for {
 		var x int
-		row := db.QueryRow(`select count(*) from INFORMATION_SCHEMA.tables where TABLE_SCHEMA="test99"`)
+		s := fmt.Sprintf("SELECT count(*) FROM information_schema.tiflash_replica where progress = 1 and table_schema = 'test99' and TABLE_NAME = '%v';", tbn)
+		row := db.QueryRow(s)
+		if err := row.Scan(&x); err != nil {
+			panic(err)
+		}
+		if x == 1 {
+			return
+		}
+	}
+}
+
+func TestPerformance(C int, T int) {
+	ctx := context.Background()
+	db := GetDB()
+	defer db.Close()
+
+	fCreate := func(ch *chan []string) {
+		for i := 0; i < C; i++ {
+			*ch <- []string{fmt.Sprintf("create table test99.t%v(z int)", i)}
+		}
+	}
+	AsyncStmt(ctx, "create", 10, db, fCreate)
+
+	collect := make([]time.Duration, 0)
+	collect2 := make([]time.Duration, 0)
+	fAlterAndWait := func(ch *chan []string) {
+		for i := 0; i < C; i++ {
+			*ch <- []string{
+				fmt.Sprintf("alter table test99.t%v set tiflash replica 1", i),
+				fmt.Sprintf("SELECT count(*) FROM information_schema.tiflash_replica where progress = 1 and table_schema = 'test99' and TABLE_NAME = 't%v';", i),
+			}
+		}
+	}
+	fChecker := func(s *[]string) {
+		var x int
+		fmt.Printf("Handle %v\n", (*s)[0])
+		start := time.Now()
+		_, err := db.Exec((*s)[0])
 		if err != nil {
 			panic(err)
 		}
-		if err = row.Scan(&x); err != nil {
-			panic(err)
+		start2 := time.Now()
+		for {
+			row := db.QueryRow((*s)[1])
+			if err = row.Scan(&x); err != nil {
+				panic(err)
+			}
+
+			if x == 1 {
+				t := time.Since(start)
+				t2 := time.Since(start2)
+				fmt.Printf("Finish %v Cost(alter+sync) %v Cost(sync) %v\n", (*s)[1], t, t2)
+				collect = append(collect, t)
+				collect2 = append(collect2, t2)
+				return
+			}
 		}
-		if x == C {
-			fmt.Printf("OK %v \v", time.Since(start))
-			break
-		}
-		fmt.Printf("---> %v\n", x)
 	}
+	AsyncStmtEx(ctx, "replica", T, db, fAlterAndWait, fChecker)
+	Summary(&collect, &collect2)
+}
+
+func Summary(collect *[]time.Duration, collect2 *[]time.Duration) {
+	fmt.Printf("Count(alter+sync) %v\n", len(*collect))
+	fmt.Printf("Count(sync) %v\n", len(*collect2))
+	total := int64(0)
+	for _, t := range *collect {
+		total += t.Milliseconds()
+	}
+	total2 := int64(0)
+	for _, t := range *collect2 {
+		total2 += t.Milliseconds()
+	}
+	delta := int64(0)
+	for i, _ := range *collect {
+		delta += (*collect2)[i].Milliseconds() - (*collect)[i].Milliseconds()
+	}
+	fmt.Printf("Avr(alter+sync) %v Avr(sync) %v Delta %v\n", float64(total*1.0)/float64(len(*collect)), float64(total2*1.0)/float64(len(*collect2)), float64(delta*1.0)/float64(len(*collect)))
 
 }
